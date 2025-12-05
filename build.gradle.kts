aaab6fb0541cfc8a98fac8fde199dad30a30e18d
Alysrazor <sergiocabades131999@gmail.com>
2025-12-05 12:55:22 +0100

    Updated Spring 3.5.7 to 4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.7"
+	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.alysrazor"
 version = "0.0.1-SNAPSHOT"
-description = "Proyecto en Spring boot que utiliza Kafka."
+description = "Proyecto en Spring boot."
 
 java {
 	toolchain {
@@ -25,23 +25,21 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-batch")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-batch:4.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:4.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-security:4.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-web:4.0.0")
     implementation("com.mysql:mysql-connector-j:8.4.0")
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	compileOnly("org.projectlombok:lombok:1.18.42")
+	annotationProcessor("org.projectlombok:lombok:1.18.42")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.batch:spring-batch-test")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:4.0.0")
+	testImplementation("org.springframework.batch:spring-batch-test:4.0.0")
+	testImplementation("org.springframework.security:spring-security-test:4.0.0")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.1")
 }
 
 tasks.withType<Test> {
